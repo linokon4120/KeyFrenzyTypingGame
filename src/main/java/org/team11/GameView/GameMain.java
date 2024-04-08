9f456299a85b23af78f957655e9402eaaed20eb8
Linh Ngo <lkn004@bucknell.edu>
2024-04-08 14:25:01 -0400

    Initializing new view and controller

---
+++ src/main/java/org/team11/GameView/GameMain.java
@@ -0,0 +1,28 @@
+/* ***************************************
+ * CSCI 205 - Software Engineering and Design
+ * Spring 2024
+ * Instructor: Prof. Lily Romano / Prof. Joshua Stough
+ *
+ * Name: Ellyn Ngo
+ * Section: 02
+ * Date: 4/8/2024
+ * Time: 2:22 PM
+ *
+ * Project: csci205_final_project
+ * Package: org.team11.GameView
+ * Class: GameMain
+ *
+ * Description:
+ *
+ * **************************************
+ */
+package org.team11.GameView;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class GameMain {
+}
+
+
+
