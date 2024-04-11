105c605ab8372668835100591a9982b00fce9bea
Rahul Sibal <rs078@bucknell.edu>
2024-04-11 15:15:54 -0400

    Updated the dictionary

--- src/main/java/org/team11/Main.java
+++ src/main/java/org/team11/Main.java
@@ -1,7 +1,18 @@
 package org.team11;
 
+import org.team11.GameView.Dictionary;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+            Dictionary dictionary = new Dictionary();
+            // Update the file path below with the correct path to your dictionary file
+
+            System.out.println(dictionary.getwords());
+            System.out.println(dictionary.getwords());
+            System.out.println(dictionary.getwords());
+            System.out.println(dictionary.getwords());
+            System.out.println(dictionary.getwords(3,4));
+
     }
 }
+
