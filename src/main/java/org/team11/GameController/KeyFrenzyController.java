1e58070fa52106807df5e530e75000d2d5bde9a6
Linh Ngo <lkn004@bucknell.edu>
2024-04-12 14:38:17 -0400

    Setting up controller and Typing

---
+++ src/main/java/org/team11/GameController/KeyFrenzyController.java
@@ -0,0 +1,22 @@
+/* ***************************************
+ * CSCI 205 - Software Engineering and Design
+ * Spring 2024
+ * Instructor: Prof. Lily Romano / Prof. Joshua Stough
+ *
+ * Name: Ellyn Ngo
+ * Section: 02
+ * Date: 4/12/2024
+ * Time: 2:37 PM
+ *
+ * Project: csci205_final_project
+ * Package: org.team11.GameController
+ * Class: KeyFrenzyController
+ *
+ * Description:
+ *
+ * **************************************
+ */
+package org.team11.GameController;
+
+public class KeyFrenzyController {
+}
