ba458e98c79da4c1c7bcd725b5bca8d03bf58a41
Linh Ngo <lkn004@bucknell.edu>
2024-04-19 15:08:24 -0400

    generate basic unit tests

---
+++ src/test/java/org/team11/GameView/WordDictionaryTest.java
@@ -0,0 +1,24 @@
+package org.team11.GameView;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WordDictionaryTest {
+
+    @Test
+    void getWords() {
+    }
+
+    @Test
+    void getWord() {
+    }
+
+    @Test
+    void guess() {
+    }
+
+    @Test
+    void getScore() {
+    }
+}
