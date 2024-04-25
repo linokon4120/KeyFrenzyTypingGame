69e97fa47fd41b5dc476d9c05c600735df71f779
Rahul Sibal <rs078@bucknell.edu>
2024-04-25 15:49:29 -0400

    I did 2 unit tests in worddictinary test

--- src/test/java/org/team11/TypingMechanism/WordDictionaryTest.java
+++ src/test/java/org/team11/TypingMechanism/WordDictionaryTest.java
@@ -1,23 +1,16 @@
-package org.team11.Ghosts;
+package org.team11.TypingMechanism;
 
 import org.junit.jupiter.api.Test;
 
 class WordDictionaryTest {
 
-    @Test
-    void getWords() {
-    }
+
 
     @Test
     void getWord() {
+
     }
 
 
-    @Test
-    void guess() {
-    }
 
-    @Test
-    void getScore() {
-    }
 }
